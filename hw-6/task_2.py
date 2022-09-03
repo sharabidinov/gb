9f707504200e2e21023bc37f8424d536c6a2d121
Sharabidinov Alymbek <alymbeksharabidinov1@gmail.com>
2022-09-03 18:55:55 +0600

    hw-6

--- hw-6/task_2.py
+++ hw-6/task_2.py
@@ -4,26 +4,13 @@
 usr_input = int(input('Введите N: '))
 
 
-def is_prime(num: int):
-    if num <= 1:
-        return False
-
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-
-    return True
-
-
-def prime_list(n):
-    result = [i for i in range(2, n + 1) if is_prime(i)]
-
-    # for i in range(2, n + 1):
-    #     if is_prime(i):
-    #         result.append(i)
-
-    return result
+def primes(num: int):
+    result = [True] * num
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if result[i]:
+            result[i * i::2 * i] = [False] * ((num - i * i - 1) // (2 * i) + 1)
+    return [2] + [i for i in range(3, num, 2) if result[i]]
 
 
 if __name__ == '__main__':
-    print(prime_list(usr_input))
+    print(primes(usr_input))
